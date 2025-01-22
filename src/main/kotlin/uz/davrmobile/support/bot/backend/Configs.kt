e653da72b6af015b72f6e50703513ac908f4e53c
nurmukhammadsobirov <sobirovn0717@gmail.com>
2025-01-22 19:05:33 +0500

    exception handling

--- src/main/kotlin/uz/davrmobile/support/bot/backend/Configs.kt
+++ src/main/kotlin/uz/davrmobile/support/bot/backend/Configs.kt
@@ -3,6 +3,9 @@ package uz.davrmobile.support.bot.backend
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.ResourceBundleMessageSource
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
+import java.util.*
 
 @Configuration
 class Configs{
@@ -14,5 +17,24 @@ class Configs{
         messageSource.setFallbackToSystemLocale(false)
         messageSource.setUseCodeAsDefaultMessage(true)
         return messageSource
+    }
+}
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    @Bean
+    fun localeResolver() = SessionLocaleResolver().apply { setDefaultLocale(Locale("en")) }
+
+    @Bean
+    fun errorMessageSource() = ResourceBundleMessageSource().apply {
+        setDefaultEncoding(Charsets.UTF_8.name())
+        setBasename("error")
+    }
+
+
+    @Bean
+    fun messageSource() = ResourceBundleMessageSource().apply {
+        setDefaultEncoding(Charsets.UTF_8.name())
+        setBasename("message")
     }
 }
