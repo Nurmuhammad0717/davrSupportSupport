54846bf39b7aca988bc0432bb129a8c33b164582
tLikWer <helloLikWer@gmail.com>
2025-01-22 16:37:18 +0500

    fix

--- src/main/kotlin/uz/davrmobile/support/bot/backend/Configs.kt
+++ src/main/kotlin/uz/davrmobile/support/bot/backend/Configs.kt
@@ -1,14 +1,18 @@
 package uz.davrmobile.support.bot.backend
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.ResourceBundleMessageSource
 
-@Bean
-fun messageSource(): ResourceBundleMessageSource {
-    val messageSource = ResourceBundleMessageSource()
-    messageSource.setBasenames("bot_messages")
-    messageSource.setDefaultEncoding("UTF-8")
-    messageSource.setFallbackToSystemLocale(false)
-    messageSource.setUseCodeAsDefaultMessage(true)
-    return messageSource
+@Configuration
+class Configs{
+    @Bean
+    fun messageSource(): ResourceBundleMessageSource {
+        val messageSource = ResourceBundleMessageSource()
+        messageSource.setBasenames("bot_messages")
+        messageSource.setDefaultEncoding("UTF-8")
+        messageSource.setFallbackToSystemLocale(false)
+        messageSource.setUseCodeAsDefaultMessage(true)
+        return messageSource
+    }
 }
