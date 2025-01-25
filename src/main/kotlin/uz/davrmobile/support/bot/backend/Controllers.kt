85f0cb0a84bcd0a158459db64030d625bb42953b
tLikWer <helloLikWer@gmail.com>
2025-01-25 12:57:07 +0500

    fix

--- src/main/kotlin/uz/davrmobile/support/bot/backend/Controllers.kt
+++ src/main/kotlin/uz/davrmobile/support/bot/backend/Controllers.kt
@@ -1,6 +1,5 @@
 package uz.davrmobile.support.bot.backend
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
@@ -14,7 +13,7 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/bot")
 class BotController(private val botService: BotService) {
-//    @IsAdmin
+    //    @IsAdmin
     @IsModerator
     @PostMapping
     fun create(@RequestBody req: TokenRequest) = botService.createBot(req)
@@ -23,17 +22,17 @@ class BotController(private val botService: BotService) {
     @GetMapping
     fun getAll() = botService.getAllBots()
 
-//    @IsAdmin
+    //    @IsAdmin
     @IsModerator
     @GetMapping("{id}")
     fun getOneBot(@PathVariable id: String) = botService.getOneBot(id)
 
-//    @IsAdmin
+    //    @IsAdmin
     @IsModerator
     @DeleteMapping("{id}")
     fun deleteBot(@PathVariable id: String) = botService.deleteBot(id)
 
-//    @IsAdmin
+    //    @IsAdmin
     @IsModerator
     @PostMapping("stop/{id}")
     fun stopBot(@PathVariable id: String) = botService.stopBot(id)
@@ -43,7 +42,7 @@ class BotController(private val botService: BotService) {
     fun getAllActiveBots() = botService.getAllActiveBots()
 
     @IsModerator
-        @PostMapping("add-bot/{id}")
+    @PostMapping("add-bot/{id}")
     fun addBot(@PathVariable id: String) = botService.addBotToOperator(id)
 
     @IsModerator
@@ -58,7 +57,8 @@ class OperatorController(
 ) {
     @IsModerator
     @GetMapping("get-sessions")
-    fun getSessions(@RequestBody @Valid request:GetSessionRequest, pageable: Pageable) = messageToOperatorService.getSessions(request,pageable)
+    fun getSessions(@RequestBody @Valid request: GetSessionRequest, pageable: Pageable) =
+        messageToOperatorService.getSessions(request, pageable)
 
     @IsModerator
     @GetMapping("get-session-messages/{id}")
@@ -86,7 +86,8 @@ class FileInfoController(
     fun upload(@RequestParam("file") multipartFile: MutableList<MultipartFile>) = fileInfoService.upload(multipartFile)
 
     @GetMapping("download/{hash-id}")
-    fun download(@PathVariable("hash-id") hashId: String, response: HttpServletResponse) = fileInfoService.download(hashId, response)
+    fun download(@PathVariable("hash-id") hashId: String, response: HttpServletResponse) =
+        fileInfoService.download(hashId, response)
 
     @GetMapping("{hash-id}")
     fun find(@PathVariable("hash-id") hashId: String) = fileInfoService.find(hashId)
